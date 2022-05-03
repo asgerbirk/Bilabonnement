e6fe4421ec1c85b04dce930a9850c29ca4162552
asgerbirk <asger.birk@gmail.com>
2022-05-03 11:35:09 +0200

    prop

--- src/main/java/com/example/bilabonnement/Service/CarService.java
+++ src/main/java/com/example/bilabonnement/Service/CarService.java
@@ -1,4 +1,20 @@
 package com.example.bilabonnement.Service;
 
+import com.example.bilabonnement.Model.Car;
+import com.example.bilabonnement.Repository.CarRepository;
+
+import java.util.List;
+
 public class CarService {
+
+    private final CarRepository carRepository;
+
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public List<Car> getAllCars(){
+        return carRepository.getAllEntities();
+    }
 }
