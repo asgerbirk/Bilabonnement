e23ea632f62c291bb5520907763c169e7eff184b
asgerbirk <asger.birk@gmail.com>
2022-05-24 15:24:46 +0200

    commit

--- src/test/java/com/example/bilabonnement/Service/CarServiceTest.java
+++ src/test/java/com/example/bilabonnement/Service/CarServiceTest.java
@@ -2,7 +2,6 @@ package com.example.bilabonnement.Service;
 
 import com.example.bilabonnement.Model.Car;
 import com.example.bilabonnement.Repository.CarRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 
 
@@ -34,10 +32,7 @@ class CarServiceTest {
          verify(carRepository).getAllEntities();
     }
 
-    @Test
-    void test(){
-        assertTrue(underTest.getAllCars().isEmpty());
-    }
+
 
     @Test
     void getPriceOfAllRentedCars() {
@@ -58,14 +53,10 @@ class CarServiceTest {
         Car notRentedCar1 = new Car(2, "bmw", "John", "black", 1000, true, false);
         Car rentedCar = new Car(2, "bmw", "John", "black", 1000, true, true);
         Car rentedCar1 = new Car(2, "bmw", "John", "black", 1000, true, true);
-        Assertions.assertEquals(this.underTest.allRentedCars().stream().allMatch((Car) -> {
-            return Car.isRented();
-        }), rentedCar.isRented() && rentedCar1.isRented());
-        Assertions.assertEquals(rentedCar.isRented(), rentedCar1.isRented());
-        Assertions.assertEquals(!notRentedCar.isRented(), !notRentedCar1.isRented());
-        Assertions.assertEquals(this.underTest.allRentedCars().stream().allMatch((Car) -> {
-            return !Car.isRented();
-        }), !notRentedCar.isRented() && !notRentedCar1.isRented());
+
+       assertEquals(rentedCar, underTest.allRentedCars().equals(rentedCar1));
+
+
     }
 
 
